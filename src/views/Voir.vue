112c2cc46393ad565071d08b14d1c4092f996050
eeldigit <eeldigit@gmail.com>
2021-11-10 13:17:50 +0100

    clean up

--- src/views/Voir.vue
+++ src/views/Voir.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="voir">
-    <!-- <Navbar /> -->
     <Voir />
   </div>
 </template>
 
 <script>
 import Voir from "@/components/Voir";
-// import Navbar from "@/components/Navbar";
 
 export default {
   components: {
     Voir,
-    // Navbar,
   },
 };
 </script>
